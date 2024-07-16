88d068e67e626937c1335ff6e8840c40e0ff5536
Aakaive <wonseok031@gmail.com>
2024-07-16 09:56:55 +0900

    install & configuration tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit', // JIT 모드 설정
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}', 
+    './main/**/*.html'
+  ],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
