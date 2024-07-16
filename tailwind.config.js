4cfb9f850558562dc716143b57baca4d81748eb6
Aakaive <wonseok031@gmail.com>
2024-07-16 15:49:55 +0900

    팀 소개란 스타일링 완성 & 반응형 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      screens: {
+        'min-1280': { 'min': '1281px'},
+        'max-1160': { 'max': '1160px'},
+        'max-lg': { 'max': '1024px' },
+        'min-lg': { 'min': '1025px' },
+        'max-md': { 'max': '768px'},
+        'max-sd': { 'max': '640px'},
+      },
+    },
   },
   plugins: [],
 }
