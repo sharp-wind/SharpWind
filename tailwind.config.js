45593f6d7a064d6406ba15ff7a1105ad6740ea1c
Aakaive <wonseok031@gmail.com>
2024-07-17 11:10:16 +0900

    temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,22 @@ module.exports = {
     './main/**/*.html'
   ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       screens: {
         'min-1280': { 'min': '1281px'},
+        'max-1280': { 'max': '1280px'},
         'max-1160': { 'max': '1160px'},
+        'max-920': { 'max': '920px'},
         'max-lg': { 'max': '1024px' },
         'min-lg': { 'min': '1025px' },
         'max-md': { 'max': '768px'},
         'max-sd': { 'max': '640px'},
       },
+    },
+  },
+  variants: {
+    extend: {
+
     },
   },
   plugins: [],
