3598a9538c8672fed888a077353d7c6d495a3602
multiverse22 <momizi527@gmail.com>
2024-07-18 17:55:28 +0900

    불필요한 파일정리

--- guestbook/src/main/java/com/example/guestbook/Repository/GuestbookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.guestbook.Repository;
-
-import com.example.guestbook.entity.Guestbook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
-}
